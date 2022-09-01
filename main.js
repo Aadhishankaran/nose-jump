73f681f6c41829a2d546b70f8b33be47418dea2c
Aadhishankaran <78032579+Aadhishankaran@users.noreply.github.com>
2022-09-01 16:48:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,35 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas')
+
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet is intitalized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist = results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+
+function startGame()
+{
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loaded"
 }
 
 
@@ -37,6 +66,13 @@ function draw(){
  fill("black");
  stroke("black");
  rect(0,0,20,700);
+
+ if(scoreRightWrist > 0.2)
+  {
+    fill("red");
+    stroke("red");
+    circle(rightWristX, rightWristY, 30);
+  }
  
    //funtion paddleInCanvas call 
    paddleInCanvas();
@@ -163,3 +199,4 @@ function paddleInCanvas(){
     mouseY =0;
   }  
 }
+
